27ee81c00a7194c69ece6075b847f3875ec3bed7
Anusuya M <anusuyamoorthy2003@gmail.com>
2026-01-29 09:59:12 +0530

    Update SMSSPAM.py

--- SMSSPAM.py
+++ SMSSPAM.py
@@ -1,14 +1,11 @@
 import streamlit as st
 import joblib
 
-# Load model + vectorizer
-model = joblib.load("final_spam_model.pkl")
-vectorizer = joblib.load("tfidf_vectorizer.pkl")
+st.set_page_config(page_title="SMS Spam Classifier")
+st.title("📩 SMS Spam Classifier")
 
-st.set_page_config(page_title="SMS Spam Classifier", page_icon="📩")
-
-st.title(" SMS Spam Classifier (NLP Project)")
-st.write("Enter an SMS message and predict whether it is **SPAM** or **HAM**.")
+model = joblib.load("final_spam_model.pkl")           
+vectorizer = joblib.load("tfidf_vectorizer.pkl")      
 
 msg = st.text_area("Enter your SMS message:")
 
@@ -16,8 +13,8 @@ if st.button("Predict"):
     if msg.strip() == "":
         st.warning(" Please enter a message.")
     else:
-        msg_vec = vectorizer.transform([msg])   
-        pred = model.predict(msg_vec)[0]        
+        msg_vec = vectorizer.transform([msg])         
+        pred = model.predict(msg_vec)[0]            
         if pred == 1:
             st.error(" SPAM Detected")
         else:
